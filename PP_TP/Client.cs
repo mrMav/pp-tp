4dad63c33ac95c29d6da568e8eeeb8bb55f9930a
DarthTRex <8bitpriest@gmail.com>
2018-01-08 15:59:02 +0000

    Added client class

---
+++ PP_TP/Client.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PP_TP
+{
+    class Client
+    {
+        string Name { get; set; }
+        string Adress { get; set; }
+        string Email { get; set; }
+        string PhoneNumber { get; set; }
+        string CC { get; set; }
+        string Nif { get; set; }
+
+        public Client (string name, string cc )
+        {
+            this.Name = name;
+            this.Adress = "";
+            this.Email = "";
+            this.PhoneNumber = "";
+            this.CC = cc;
+            this.Nif = "";
+        }
+    }
+}
