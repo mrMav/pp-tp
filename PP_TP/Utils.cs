441b1cc4b55bf64a19a6e25625458de2e8ca47f2
mrmav <mrmav@outlook.pt>
2018-01-08 20:05:38 +0000

    added a Utils static class for error printing

---
+++ PP_TP/Utils.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PP_TP
+{
+    public static class Utils
+    {
+
+        public static void PrintError(string s)
+        {
+
+            Console.ForegroundColor = System.ConsoleColor.Red;
+
+            Console.WriteLine(s);
+
+            Console.ForegroundColor = System.ConsoleColor.Green;
+
+        }
+
+    }
+}
