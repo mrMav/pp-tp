8052813d72a4e71da85b0da45c16e2b2bf8c7834
João Francisco Santos <a14840@alunos.ipca.pt>
2018-01-08 15:40:51 +0000

    FIxed Purchase?

---
+++ PP_TP/Purchase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PP_TP
+{
+    class Purchase
+    {
+        public List<Product> Products { get; set; }
+        public string Description { get; set; }
+        public DateTime PurchaseDate { get; set; }
+        public int Quantity { get; set; }
+        public float PurchaseTotal { get; set; }
+        public int AccumulatedPoints { get; set; }
+
+        public void Purchases(List<Product> products, string desc, int quant, float pTotal, int accPoints)
+        {
+            this.Description = desc;
+            this.PurchaseDate = DateTime.Now;
+            this.Quantity = quant;
+            this.PurchaseTotal = pTotal;
+            this.AccumulatedPoints = accPoints;
+
+        }
+
+    }
+}
