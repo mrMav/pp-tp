71bcb0c015158818854d0bd2e2979e9d0c535ab3
João Francisco Santos <a14840@alunos.ipca.pt>
2018-01-08 15:07:59 +0000

    New class Purchase.cs

---
+++ PP_TP/Purchase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PP_TP
+{
+    class Purchase
+    {
+        public List<Product> products { get; set; }
+        public string ClienteC { get; set; }
+        public DateTime DataCompra { get; set; }
+        public string TipoPagamento { get; set; }
+        public bool EstadoPagamento { get; set; }
+        public string VinhosComprados { get; set; }
+        public float CustoTotal { get; set; }
+
+    }
+}
